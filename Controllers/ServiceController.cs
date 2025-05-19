aea087ca2fbce8429667b344d71b307ea847cf1c
Boitshoko Mphahlele <boitshokontshoekgolo@gmail.com>
2025-05-19 16:23:23 +0200

    Jack

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -14,7 +14,32 @@ namespace RescueBus.Controllers
             return View();
         }
 
-        public ActionResult BookingView()
+        public ActionResult ALSView()
+        {
+            return View();
+        }
+
+        public ActionResult BLSView()
+        {
+            return View();
+        }
+
+        public ActionResult AirAmbulanceView()
+        {
+            return View();
+        }
+
+        public ActionResult EMAView()
+        {
+            return View();
+        }
+
+        public ActionResult MUVView()
+        {
+            return View();
+        }
+
+        public ActionResult PatientTransportView()
         {
             return View();
         }
---
+++ Models/Booking.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RescueBus.Models
+{
+    public class Booking
+    {
+        private string _ClientName;
+        private string _ClientPhone;
+        private string _ClientAddress;
+        private string _Driver;
+        private string _Vehicle;
+        private string _Reason;
+        private string _PickUpTime;
+
+        //Constructor
+        public Booking()
+        {
+            string ClientName = _ClientName;
+            string ClientPhone = _ClientPhone;
+            string ClientAddress = _ClientAddress;
+            string Driver = _Driver;
+            string Vehicle = _Vehicle;
+            string reason = _Reason;
+            string pickUpTime = _PickUpTime;
+          
+        }
+
+        //Properties
+        public string ClientName
+        {
+            get { return _ClientName; }
+            set { _ClientName = value; }
+        }
+        public string ClientPhone
+        {
+            get { return _ClientPhone; }
+            set { _ClientPhone = value; }
+        }
+        public string ClientAddress
+        {
+            get { return _ClientAddress; }
+            set { _ClientAddress = value; }
+        }
+        public string Driver
+        {
+            get { return _Driver; }
+            set { _Driver = value; }
+        }
+        public string Vehicle
+        {
+            get { return _Vehicle; }
+            set { _Vehicle = value; }
+        }
+        public string reason
+        {
+            get { return _Reason; }
+            set { _Reason = value; }
+        }
+        public string PickUpTime
+        {
+            get { return _PickUpTime; }
+            set { _PickUpTime = value; }
+        }
+    }
+}
